e27a61ae78f29012c6f15a3443684e7fc051494d
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-27 18:56:59 +0200

    create videos controller

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,16 @@
-<h1 class="title is-1">Home</h1>
+<script>
+  import { getVideos } from "../lib/controllers/videos";
+</script>
+
+<div class="container">
+  <h1 class="title is-1">Home</h1>
+  {#await getVideos()}
+    <strong>loading</strong>
+  {:then videos}
+    <pre>
+      {JSON.stringify(videos, null, 2)}
+    </pre>
+  {:catch e}
+    <pre>Something went wrong</pre>
+  {/await}
+</div>
