0cdc1ec667f3d6652e1ee888e892c7eab17d4e73
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-28 11:37:44 +0200

    remove monkey patching

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
-import { monkeyPatchFetch } from "./lib/fetch-interceptor.js";
-
-monkeyPatchFetch();
 
 const app = mount(App, {
   target: document.getElementById("app"),
