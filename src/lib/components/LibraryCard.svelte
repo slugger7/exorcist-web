1b91b36486a383c0e3b1471801a1b6acde04a958
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-30 12:52:04 +0200

    update library card to create library paths

--- src/lib/components/LibraryCard.svelte
+++ src/lib/components/LibraryCard.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { Link } from "svelte-routing";
+  import routes from "../../routes/routes";
+
   /** @type {{ name: String, id: string }}*/
   let { name, id } = $props();
 </script>
@@ -6,10 +9,15 @@
 <div class="card">
   <header class="card-header">
     <p class="card-header-title">{name}</p>
-    <button class="card-header-icon" aria-label="more-options">
+    <Link
+      class="card-header-icon"
+      aria-label="more-options"
+      to={routes.create.libraryPathFunc(id)}
+      state={{ libraryName: name }}
+    >
       <span class="icon">
         <i class="fa-solid fa-plus" aria-hidden="true"></i>
       </span>
-    </button>
+    </Link>
   </header>
 </div>
