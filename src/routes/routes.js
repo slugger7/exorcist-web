6012cd956671154e1e11b9186c73b21d7ad7ca28
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-30 12:51:06 +0200

    create route and rout func for library path creation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,13 @@ const routes = {
     login: "/login",
     logout: "/logout",
     create: {
-        library: "/create/library"
+        library: "/create/library",
+        libraryPath: "/create/library-path/:libraryId",
+        /** 
+         * @param {string} id
+         * @returns {string}
+        */
+        libraryPathFunc: (id) => (`/create/library-path/${id}`)
     }
 }
 export default routes;
