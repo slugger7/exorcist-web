c0d32339e8daa49303480c36c3d7fb3b4653da26
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-04-30 08:48:00 +0200

    create library card and render libraries

---
+++ src/lib/components/LibraryCard.svelte
@@ -0,0 +1,15 @@
+<script>
+  /** @type {{ name: String, id: string }}*/
+  let { name, id } = $props();
+</script>
+
+<div class="card">
+  <header class="card-header">
+    <p class="card-header-title">{name}</p>
+    <button class="card-header-icon" aria-label="more-options">
+      <span class="icon">
+        <i class="fa-solid fa-plus" aria-hidden="true"></i>
+      </span>
+    </button>
+  </header>
+</div>
--- src/routes/Libraries.svelte
+++ src/routes/Libraries.svelte
@@ -2,6 +2,7 @@
   import { getLibraries } from "../lib/controllers/libraries";
   import HeaderIconLink from "../lib/components/HeaderIconLink.svelte";
   import routes from "./routes.js";
+  import LibraryCard from "../lib/components/LibraryCard.svelte";
 </script>
 
 <div class="container">
@@ -12,13 +13,19 @@
     to={routes.create.library}
   />
 
-  {#await getLibraries()}
-    <span>loading</span>
-  {:then libraries}
-    <pre>{JSON.stringify(libraries, null, 2)}</pre>
-  {:catch}
-    <span>something went wrong</span>
-  {/await}
+  <div class="section">
+    {#await getLibraries()}
+      <span>loading</span>
+    {:then libraries}
+      {#each libraries as { id, name } (id)}
+        <LibraryCard {name} {id} />
+      {:else}
+        <p>No libraries. Create one</p>
+      {/each}
+    {:catch}
+      <p>Oops! Could not get libraries</p>
+    {/await}
+  </div>
 </div>
 
 <style>
