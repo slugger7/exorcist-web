4628db306849c245c836738da28eca95a145eca3
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-01 17:37:09 +0200

    pagination logic

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -1,22 +1,29 @@
 <script>
-    /**
-     * @typedef props
-     * @type {object}
-     * @property {number} [page]
-     * @property {number} [limit]
-     * @property {number} [skip]
-     * @property {number} total
-     * @property {string} url
-     */
+  /**
+   * @typedef props
+   * @type {object}
+   * @property {number} [page]
+   * @property {number} [limit]
+   * @property {number} [skip]
+   * @property {number} total
+   * @property {string} url
+   */
 
-     /** @type {props}*/
-    let {page = 1, limit = 48, skip = 0, total, url} = $props()
-    let pages = 3
-    const urlForPage = (page) => (`${url}/?page=${page}&limit=${limit}&skip=${skip}`)
+  /** @type {props}*/
+  let { page = 10, limit = 48, skip = 0, total, url } = $props();
+  let pages = 10;
+  const urlForPage = (page) =>
+    `${url}/?page=${page}&limit=${limit}&skip=${skip}`;
 </script>
 
 {#snippet pageLine(pn)}
-<li><a href={urlForPage(pn)} class="pagination-link {page === pn ? "is-current":""}" aria-label="Goto page {pn}">{pn}</a></li>
+  <li>
+    <a
+      href={urlForPage(pn)}
+      class="pagination-link {page === pn ? 'is-current' : ''}"
+      aria-label="Goto page {pn}">{pn}</a
+    >
+  </li>
 {/snippet}
 
 <nav class="pagination is-centered" aria-label="pagination">
@@ -25,31 +32,36 @@
   <ul class="pagination-list">
     {@render pageLine(1)}
 
-    {#if page <= 3 && pages > 3}
+    {#if page <= 3}
       {@render pageLine(2)}
       {@render pageLine(3)}
     {/if}
 
-    {#if pages > 5 && page > 3} 
-        <li><span class="pagination-ellipsis">&hellip;</span></li>
+    {#if page > 3 && pages > 5}
+      <li><span class="pagination-ellipsis">&hellip;</span></li>
+      {#if pages - page >= 3}
         {@render pageLine(page - 1)}
-        
+      {/if}
     {/if}
 
-    {#if page > 3}
+    {#if pages > 5 && page > 3 && pages - page >= 3}
       {@render pageLine(page)}
     {/if}
 
-    {#if pages - page > 3 && page != pages}
-      {@render pageLine(page + 1)}
+    {#if pages - page >= 3 && pages > 5}
+      {#if page > 3}
+        {@render pageLine(page + 1)}
+      {/if}
       <li><span class="pagination-ellipsis">&hellip;</span></li>
     {/if}
 
-    {#if pages - page <= 3 && pages > 3}
+    {#if pages - page < 3 && pages > 5}
       {@render pageLine(pages - 2)}
       {@render pageLine(pages - 1)}
     {/if}
 
-    {@render pageLine(pages)}
+    {#if pages > 3}
+      {@render pageLine(pages)}
+    {/if}
   </ul>
 </nav>
