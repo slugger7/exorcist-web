3d08cd4440c8d0a167254cd3dea1478fcfdd4194
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-01 20:57:47 +0200

    change video image to link

--- src/lib/components/VideoCard.svelte
+++ src/lib/components/VideoCard.svelte
@@ -1,22 +1,28 @@
 <script>
     import { Link } from "svelte-routing";
-  import routes from "../../routes/routes";
-  import { imageUrlById } from "../controllers/image";
+    import routes from "../../routes/routes";
+    import { imageUrlById } from "../controllers/image";
 
     /** @import { Video } from "../types"*/
 
     /** @type {{video: Video}} */
-    let { video } = $props()
+    let { video } = $props();
 </script>
 
 <div class="card">
     <div class="card-image">
         <figure class="image is16by9">
-            <img src={imageUrlById(video.thumbnailId)} alt={video.title} />
+            <Link to={routes.videoFunc(video.id)}
+                ><img
+                    src={imageUrlById(video.thumbnailId)}
+                    alt={video.title}
+                /></Link
+            >
         </figure>
     </div>
     <header class="card-header">
-        <Link class="card-header-title" to={routes.videoFunc(video.id)}>{video.title}</Link>
-
+        <Link class="card-header-title" to={routes.videoFunc(video.id)}
+            >{video.title}</Link
+        >
     </header>
 </div>
