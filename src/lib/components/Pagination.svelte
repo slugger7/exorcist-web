e3d146a6bcd0378d9334f9ea93d3619a0bff3d04
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-05 20:09:00 +0200

    fix pagination bug

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -5,9 +5,8 @@
    * @property {number} page
    * @property {number} limit
    * @property {number} total
-   * @property {string} url
+   * @property {string} [url]
    */
-
   import { Link } from "svelte-routing";
 
   /** @type {props}*/
@@ -50,10 +49,10 @@
   <ul class="pagination-list">
     {@render pageLine(1)}
 
-    {#if page <= 3}
-      {@render pageLine(2)}
-      {@render pageLine(3)}
-      {@render pageLine(4)}
+    {#if page <= 3 && pages > 1}
+      {#each { length: pages - 1 }, pn}
+        {@render pageLine(pn + 2)}
+      {/each}
     {/if}
 
     {#if page > 3 && pages > 5}
