f914c7b00843e3b4edde8f9d3f33bbc9844e6ff3
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-07 13:08:17 +0200

    remove custom response mocks

--- src/lib/controllers/fetch.js
+++ src/lib/controllers/fetch.js
@@ -12,17 +12,8 @@ export async function fetch(resource, config = null) {
   if (config.credentials === undefined || config.credentials === null) {
     config.credentials = "include"
   }
-const actualFetch = async () => await window.fetch(resource, config);
 
-   /** @type {Response} */
-  let response
-  if (import.meta.env.DEV) {
-    const mocks = await import("./mocks")
-   
-    return await mocks.mock(resource, actualFetch)
-  } else {
-    response = await actualFetch()
-  }
+  const response = await window.fetch(resource, config)
 
   if (response.status == 401) {
     navigate(`/login?returnUrl=${encodeURIComponent(window.location.pathname)}${window.location.search}`)
