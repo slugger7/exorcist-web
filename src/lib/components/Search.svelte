7f864966db3cd09cf1514f902ac1355752d122f5
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-05-12 18:12:37 +0200

    create search component and debounce result

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,28 @@
+<script>
+  /**
+   * @import { ChangeEventHandler } from "svelte/elements";
+   *
+   * @typedef props
+   * @type {object}
+   * @property {string} [value]
+   * @property {ChangeEventHandler<HTMLInputElement>} onkeyup
+   */
+
+  /** @type {props} */
+  let { value = $bindable(""), onkeyup } = $props();
+</script>
+
+<div class="field">
+  <p class="control has-icons-left">
+    <input
+      class="input"
+      type="text"
+      placeholder="Search"
+      bind:value
+      {onkeyup}
+    />
+    <span class="icon is-left">
+      <i class="fas fa-search"></i>
+    </span>
+  </p>
+</div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,16 +1,37 @@
 <script>
+  import { navigate } from "svelte-routing";
   import Pagination from "../lib/components/Pagination.svelte";
+  import Search from "../lib/components/Search.svelte";
   import VideoCard from "../lib/components/VideoCard.svelte";
   import { getVideos } from "../lib/controllers/videos";
-  import { getIntSearchParamOrDefault } from "../lib/searchParams";
+  import {
+    getIntSearchParamOrDefault,
+    getStringSearchParamOrDefault,
+  } from "../lib/searchParams";
+  import routes from "./routes";
 
   let page = $state(getIntSearchParamOrDefault("page", 1));
   let limit = $state(getIntSearchParamOrDefault("limit", 48));
+  let search = $state(getStringSearchParamOrDefault("search", ""));
+
+  let searchTimeout;
+  const onSearchChange = (event) => {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      search = event.target.value;
+      const params = new URLSearchParams(window.location.search);
+      params.set("search", search);
+      navigate(`${routes.home}?${params.toString()}`);
+    }, 500);
+  };
 </script>
 
 <div class="container is-fluid">
   <h1 class="title is-1">Home</h1>
-  {#await getVideos(page, limit)}
+  <div class="block">
+    <Search onkeyup={onSearchChange} value={search} />
+  </div>
+  {#await getVideos(page, limit, search)}
     <strong>loading</strong>
   {:then videosPage}
     <div class="grid is-col-min-15">
