25c107ed36440dd447d53697c4e307b02a453766
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-06-06 15:29:17 +0200

    update video.svelte to editorconfig

--- src/routes/Video.svelte
+++ src/routes/Video.svelte
@@ -1,39 +1,36 @@
 <script>
-    import { onDestroy, onMount } from "svelte";
-    import { imageUrlById } from "../lib/controllers/image";
-    import { videoUrlById, get } from "../lib/controllers/media";
+  import { onDestroy } from "svelte";
+  import { imageUrlById } from "../lib/controllers/image";
+  import { videoUrlById, get } from "../lib/controllers/media";
 
-    /** @type {{id: string}}*/
-    let { id } = $props();
+  /** @type {{id: string}}*/
+  let { id } = $props();
 
-    onDestroy(() => {
-        localStorage.setItem("item", id);
-    });
+  onDestroy(() => {
+    localStorage.setItem("item", id);
+  });
 </script>
 
 {#await get(id)}
-    <p>loading</p>
+  <p>loading</p>
 {:then { thumbnailId, title }}
-    <div class="container is-fluid">
-        <!-- svelte-ignore a11y_media_has_caption -->
-        <video
-            src={videoUrlById(id)}
-            controls
-            poster={imageUrlById(thumbnailId)}
-        ></video>
+  <div class="container is-fluid">
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <video src={videoUrlById(id)} controls poster={imageUrlById(thumbnailId)}
+    ></video>
 
-        <div class="container">
-            <h1 class="title is-1">{title}</h1>
-        </div>
+    <div class="container">
+      <h1 class="title is-1">{title}</h1>
     </div>
+  </div>
 {:catch}
-    <p>something went wrong</p>
+  <p>something went wrong</p>
 {/await}
 
 <style>
-    video {
-        height: 100%;
-        width: 100%;
-        max-height: 90vh;
-    }
+  video {
+    height: 100%;
+    width: 100%;
+    max-height: 90vh;
+  }
 </style>
