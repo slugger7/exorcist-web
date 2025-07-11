b71a304e9a32bf9bdf6ef120b8c6eec48ea98916
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-11 07:48:17 +0200

    set up ws topic for home page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,42 @@
 <script>
+  /**
+   * @import { MediaOverviewDTO } from "../dto"
+   * @import { WSTopicMapView } from "../lib/types"
+   */
   import { getAllWithParams, ordinals } from "../lib/controllers/media";
   import routes from "./routes";
   import MediaView from "../lib/components/MediaView.svelte";
+
+  /** @type {WSTopicMapView<MediaOverviewDTO>}*/
+  const topics = {
+    video_create: (mediaPage, newItems, media) => {
+      newItems.push(media);
+
+      return [mediaPage, newItems];
+    },
+    video_update: (mediaPage, newItems, updatedMedia) => {
+      newItems = newItems.map((v) => {
+        if (v.id === updatedMedia.id) {
+          return { ...v, ...updatedMedia };
+        }
+        return v;
+      });
+
+      if (mediaPage) {
+        mediaPage.data = mediaPage.data.map((v) => {
+          if (v.id === updatedMedia.id) {
+            return {
+              ...v,
+              ...updatedMedia,
+            };
+          }
+          return v;
+        });
+      }
+
+      return [mediaPage, newItems];
+    },
+  };
 </script>
 
 <MediaView
@@ -9,4 +44,5 @@
   fetchFn={getAllWithParams}
   route={routes.home}
   {ordinals}
+  topicMap={topics}
 />
