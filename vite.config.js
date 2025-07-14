2aefa5a9bc05e5b3f3e12d60b262567f83825b15
Kevin David Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-14 15:43:44 +0200

    Chore/package-cleanup (#36)
    * move packages
    
    * remove unused packages from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    // basicSsl()
-    ],
+  ],
 })
