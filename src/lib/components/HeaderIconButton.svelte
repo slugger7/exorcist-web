84c5eb9b26a22a2bc1c8ee9aa21fe707b093a8d1
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-30 09:54:13 +0200

    add disabled to header icon button

--- src/lib/components/HeaderIconButton.svelte
+++ src/lib/components/HeaderIconButton.svelte
@@ -7,14 +7,27 @@
    * @property {string} ariaLabel
    * @property {string} [iconClass]
    * @property {string} [buttonClass]
+   * @property {boolean} [disabled]
    * @property {MouseEventHandler<HTMLButtonElement>} onclick
    */
 
   /** @type {props}*/
-  let { icon, ariaLabel, onclick, iconClass = "", buttonClass = "" } = $props();
+  let {
+    icon,
+    ariaLabel,
+    onclick,
+    iconClass = "",
+    buttonClass = "",
+    disabled = false,
+  } = $props();
 </script>
 
-<button class={`mt-2 ml-1 button inline ${buttonClass}`} aria-label={ariaLabel} {onclick}>
+<button
+  class={`mt-2 ml-1 button inline ${buttonClass}`}
+  aria-label={ariaLabel}
+  {onclick}
+  {disabled}
+>
   <span class={`icon ${iconClass}`}>
     <i class={icon}></i>
   </span>
