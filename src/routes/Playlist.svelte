5516e8a3ae58bee9e0798581678dffaf782729e3
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-07-31 14:41:27 +0200

    update playlist title

--- src/routes/Playlist.svelte
+++ src/routes/Playlist.svelte
@@ -2,14 +2,41 @@
   import MediaView from "../lib/components/MediaView.svelte";
   import routes from "./routes";
   import { ordinals } from "../lib/controllers/media";
-  import { getMediaWithParams } from "../lib/controllers/playlists";
+  import {
+    getMediaWithParams,
+    updatePlaylist,
+  } from "../lib/controllers/playlists";
+  import { navigate } from "svelte-routing";
 
   let { id, name } = $props();
+  let title = $state(name);
+  let submittingTitle = $state(false);
+
+  const updateTitle = async (newTitle) => {
+    submittingTitle = true;
+    try {
+      const updatedTitle = await updatePlaylist(id, newTitle);
+
+      title = updatedTitle.name;
+
+      const params = new URLSearchParams(location.search);
+
+      navigate(`${routes.playlistFn(id, title)}?${params.toString()}`, {
+        replace: true,
+      });
+    } finally {
+      submittingTitle = false;
+    }
+
+    return title;
+  };
 </script>
 
 <MediaView
-  title={name}
+  {title}
   route={routes.playlistFn(id, name)}
   {ordinals}
   fetchFn={async (params) => await getMediaWithParams(id, params)}
+  {updateTitle}
+  bind:submittingTitle
 />
