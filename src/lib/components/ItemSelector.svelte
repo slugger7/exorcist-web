c382c6ef15d4a8f3e2596b7239bcad8e95b40b24
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-08-02 18:06:01 +0200

    improve sector next node blur

--- src/lib/components/ItemSelector.svelte
+++ src/lib/components/ItemSelector.svelte
@@ -41,7 +41,7 @@
   let showCreateItem = $derived(
     !disableCreate &&
       query.length >= 1 &&
-      !items.find((item) => item.name === query),
+      !items.find((item) => item.name.toLowerCase() === query.toLowerCase()),
   );
   let selectionBoundary = $derived(
     showCreateItem ? itemsInView.length : itemsInView.length - 1,
@@ -56,8 +56,6 @@
       inputNode.focus();
     }
   });
-
-  $inspect(active, itemsInView);
 
   $effect(() => {
     itemsInView = items.slice(0, itemsInViewCount);
@@ -116,6 +114,11 @@
   const handleEscape = () => {
     active = false;
     inputNode.blur();
+
+    if (nextFocusState.node) {
+      nextFocusState.node.focus();
+      nextFocusState.node = inputNode;
+    }
   };
 
   const onKeyDown = (e) => {
@@ -154,7 +157,7 @@
     );
     if (exact) {
       itemsInView = itemsInView.filter(
-        (t) => t.name.toLowerCase() !== exact.name,
+        (t) => t.name.toLowerCase() !== exact.name.toLowerCase(),
       );
       itemsInView = [exact, ...itemsInView];
 
@@ -180,18 +183,6 @@
     create(query);
     reset();
   };
-
-  /** @param {KeyboardEvent} e*/
-  const handleOnKeyUp = (e) => {
-    switch (e.code) {
-      case "Escape":
-        if (nextFocusState.node) {
-          nextFocusState.node.focus();
-          nextFocusState.node = inputNode;
-        }
-        break;
-    }
-  };
 </script>
 
 <div class="field is-grouped">
@@ -206,7 +197,6 @@
           onfocus={onDropdownFocus}
           onblur={onDropdownBlur}
           onkeydown={onKeyDown}
-          onkeyup={handleOnKeyUp}
           oninput={onQueryChange}
           bind:this={inputNode}
         />
