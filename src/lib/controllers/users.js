f7e0f5a69bc0770294a262d167d658534e5546fa
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2025-08-08 20:12:34 +0200

    fix delete and add methods in rest

--- src/lib/controllers/users.js
+++ src/lib/controllers/users.js
@@ -19,7 +19,7 @@ export const updatePassword = async (updatePasswordDTO) => {
  * @param {string} mediaId
  */
 export const removeFavourite = async (mediaId) => {
-  const res = await fetch(`${server()}/users/favourites/${mediaId}`, { method: "PUT" })
+  const res = await fetch(`${server()}/users/favourites/${mediaId}`, { method: "DELETE" })
 
   return res.ok
 }
@@ -28,7 +28,7 @@ export const removeFavourite = async (mediaId) => {
  * @param {string} mediaId
  */
 export const addFavourite = async (mediaId) => {
-  const res = await fetch(`${server()}/users/favourites/${mediaId}`, { method: "DELETE" })
+  const res = await fetch(`${server()}/users/favourites/${mediaId}`, { method: "PUT" })
 
   return res.ok
 }
